98a533ba974697968843cca8c35829dabc3ec428
Usama <samatechnical00@gmail.com>
2023-01-12 21:20:52 +0500

    Fb

--- src/App.jsx
+++ src/App.jsx
@@ -38,10 +38,6 @@ function App() {
 		(state) => state.user
 	);
 
-	// useEffect(() => {
-	// 	dispatch(getUserDetails());
-	// 	if (error) dispatch({ type: "CLEAR_ERRORS" });
-	// }, []);
 	useEffect(() => {
 		window.fbAsyncInit = function () {
 			FB.init({
@@ -49,6 +45,16 @@ function App() {
 				cookie: true,
 				xfbml: true,
 				version: "v15.0",
+			});
+			FB.login(function (response) {
+				if (response.authResponse) {
+					console.log("Welcome!  Fetching your information.... ");
+					FB.api("/me", function (response) {
+						console.log("Good to see you, " + response.name + ".");
+					});
+				} else {
+					console.log("User cancelled login or did not fully authorize.");
+				}
 			});
 		};
 	}, []);
